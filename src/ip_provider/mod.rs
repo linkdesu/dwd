22fbd9ca2f76ab38243eb8a1b462f0ac3e84ab63
Link <xieaolin@gmail.com>
2022-05-15 11:24:50 +0800

    feat: add vnet.one as a new IP provider

--- src/ip_provider/mod.rs
+++ src/ip_provider/mod.rs
@@ -1,13 +1,14 @@
 //! This is public IP provider module, take a look at myip_la.rs or myip_ipip_net.rs then your will
 //! know how to add your own provider.
 //! At last don't forget to add it to the `fn get_ip` below.
-use log::{error, debug};
+use log::{debug, error};
 use std::process;
 
 use super::util::{error_style, info_style};
 
 mod myip_ipip_net;
 mod myip_la;
+mod vnet_one;
 
 /// Get public IP from different provider.
 pub async fn get_ip(provider: &str) -> Result<String, ()> {
@@ -16,6 +17,7 @@ pub async fn get_ip(provider: &str) -> Result<String, ()> {
     let ret = match provider {
         "myip.la" => myip_la::get_ip().await,
         "myip.ipip.net" => myip_ipip_net::get_ip().await,
+        "vnet.one" => vnet_one::get_ip().await,
         _ => {
             error!(target: "error", "Provider {} does not supported.", error_style(provider));
             process::exit(1);
---
+++ src/ip_provider/vnet_one.rs
@@ -0,0 +1,29 @@
+use std::error::Error;
+
+use crate::util::get;
+
+/// Get public IP from https://ip.vnet.one/check.php
+///
+/// The homepage of the provider is https://www.vnet.one/ .
+///
+/// # Example:
+///
+/// ```rust
+/// let ip = vnet_one::get_ip().await?;
+/// ```
+pub async fn get_ip() -> Result<String, Box<dyn Error>> {
+    get("https://ip.vnet.one/check.php").await.map_err(|e| e.into())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::util::is_ip;
+
+    #[tokio::test]
+    async fn get_ip_should_works() {
+        let ret = get_ip().await;
+        assert!(ret.is_ok());
+        assert!(is_ip(ret.as_ref().unwrap()));
+    }
+}
